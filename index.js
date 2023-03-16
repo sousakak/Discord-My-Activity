74ff88f046e7577736735da7634baa6c761e0d34
sousakak7 <sousakak7@gmail.com>
2023-03-16 11:07:20 +0900

    new commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var discord_rpc = require("discord-rpc")
+const client = new discord_rpc.Client({
+    transport: 'ipc'
+})
+client.on("ready", () => {
+    client.setActivity({
+        state: "Solo Traveling",
+        details: "The Journey of Elaina",
+        assets: {
+            "large-image": "title-image",
+            "large-text": "On the road to travel",
+            "small-image": "small-image",
+            "small-text": "Level 87"
+        },
+        buttons: [{
+            label : "Official Web Site",
+            url: "https://majotabi.jp/"
+        }]
+    });
+});
+client.login({clientId : "1073762890445766656"}).catch(console.error);
